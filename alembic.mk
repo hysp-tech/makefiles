66214bb9d2841dfa30cb9ea4aaaf34d406481702
ZF <z@hysp.org>
2024-11-24 10:08:47 -0800

    update

--- alembic.mk
+++ alembic.mk
@@ -1,35 +1,23 @@
-.PHONY: alembic-init alembic-revision alembic-upgrade docker-alembic-revision docker-alembic-upgrade
+.PHONY: alembic-init alembic-revision alembic-upgrade
+
+DB_APP ?= app
 
 alembic-init:  ## inital alembic db env
 	alembic init alembic
 
-alembic-revision:  ## Create a new alembic revision file, e.g make alembic-revision REV_LOG="add user table"
+alembic-revision:  ## Create a new alembic revision file[within a docker app env], e.g make alembic-revision REV_LOG="add user table" DB_APP=app
 	@if [ -z "$(REV_LOG)" ]; then \
 		echo "Error: variable REV_LOG is required."; \
 		exit 1; \
 	fi
-	alembic revision --autogenerate -m "$(REV_LOG)"
 
-alembic-upgrade: ## Apply alembic revision, e.g make alembic-upgrade REV=xyz
+	docker-compose build $(DB_APP) 
+	docker-compose run $(DB_APP) poetry run alembic revision --autogenerate -m "$(REV_LOG)"
+
+
+alembic-upgrade: ## Apply alembic revision, e.g make alembic-upgrade REV=xyz DB_APP=app
 	@if [ -z "$(REV)" ]; then \
 		echo "Error: variable REV is required for the target"; \
 		exit 1; \
 	fi
-	alembic upgrade $(REV)
-
-
-docker-alembic-revision:
-	@if [ -z "$(REV_LOG)" ]; then \
-		echo "Error: variable REV_LOG is required."; \
-		exit 1; \
-	fi
-	docker-compose build app 
-	docker-compose run app poetry run alembic revision --autogenerate -m "$(REV_LOG)"
-
-
-docker-alembic-upgrade: 
-	@if [ -z "$(REV)" ]; then \
-		echo "Error: variable REV is required for the target"; \
-		exit 1; \
-	fi
-	docker-compose run app poetry run alembic upgrade $(REV)
+	docker-compose run $(DB_APP) poetry run alembic upgrade $(REV)
