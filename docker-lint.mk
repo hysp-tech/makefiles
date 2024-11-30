13354d275e7e9380be64120c3415d776788bcf16
ZF <z@hysp.org>
2024-11-30 12:31:35 -0800

    update docker mk

--- docker-lint.mk
+++
@@ -1,7 +0,0 @@
-.PHONY: docker-lint docker-fmt
-
-docker-lint:  ## Lint the code folder
-	docker compose -f docker-compose-ci.yml up --build lint
-
-docker-fmt:  ## Apply python formatter (will edit the code)
-	CURRENT_UID=$$(id -u):$$(id -g) docker compose -f docker-compose-ci.yml up --build fmt
--- docker.mk
+++ docker.mk
@@ -1,13 +1,15 @@
 .PHONY: docker-compose-build docker-compose-up docker-compose-shell docker-compose-bash
 
-docker-compose-build:  ## Build the app
-	docker compose build
+COMPOSE_FILE ?= docker-compose.yml
 
-docker-compose-up:  ## Run app with rebuild
-	docker compose up --build
+docker-compose-build:  ## Build the app
+	docker compose -f $(COMPOSE_FILE) build $(APP)
+
+docker-compose-up:  ## e.g docker-compose-up APP=api [COMPOSE_FILE=/a/b/c.yml]
+	docker compose -f $(COMPOSE_FILE) up --build $(APP) 
 
 docker-compose-shell: # Run shell backend
-	docker compose up -d --build shell
+	docker compose -f $(COMPOSE_FILE) up -d --build shell
 
 docker-compose-bash: docker-compose-shell  ## Connect to a bash within the docker image
-	docker compose exec shell bash
+	docker compose -f $(COMPOSE_FILE) exec shell bash
