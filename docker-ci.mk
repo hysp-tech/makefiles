5dbc04dc3b318b73c05dd61b29769b454d4725d4
ZF <z@hysp.org>
2025-01-11 12:50:37 -0800

    add comments

--- docker-ci.mk
+++ docker-ci.mk
@@ -6,7 +6,7 @@ docker-ci-shell: # Create CI shell backend
 docker-ci-bash: ci-shell  # Connect to a bash within the tool image(faster), for running task like `poetry lock`
 	docker compose -f docker-compose-ci.yml exec shell bash
 
-docker-test: ## Run unit test
+docker-test: ## Run unit test, e.g make docker-test TEST_FILES='./a/x.py ./a/y.py', default to run all tests.
 	@echo "Running unit tests..."
 	docker compose -f docker-compose-ci.yml build test
 	docker compose -f docker-compose-ci.yml run --rm -e TEST_FILES=$(TEST_FILES) test
