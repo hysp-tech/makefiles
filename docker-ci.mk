fc55c22500865b01b662440f4e92210138567f24
ZF <z@hysp.org>
2025-01-13 17:37:34 -0800

    configurable docker file for ci

--- docker-ci.mk
+++ docker-ci.mk
@@ -1,17 +1,20 @@
 .PHONY: docker-ci-shell docker-ci-bash docker-test docker-int-test
 
+# Default to use 'docker-compose-ci.yml'
+CI_DOCKER_COMPOSE_FILE := docker-compose-ci.yml
+
 docker-ci-shell: # Create CI shell backend
-	docker compose -f docker-compose-ci.yml up --build -d shell
+	docker compose -f $(CI_DOCKER_COMPOSE_FILE) up --build -d shell
 
 docker-ci-bash: ci-shell  # Connect to a bash within the tool image(faster), for running task like `poetry lock`
-	docker compose -f docker-compose-ci.yml exec shell bash
+	docker compose -f $(CI_DOCKER_COMPOSE_FILE) exec shell bash
 
 docker-test: ## Run unit test, e.g make docker-test TEST_FILES='./a/x.py ./a/y.py', default to run all tests.
 	@echo "Running unit tests..."
-	docker compose -f docker-compose-ci.yml build test
-	docker compose -f docker-compose-ci.yml run --rm -e TEST_FILES=$(TEST_FILES) test
+	docker compose -f $(CI_DOCKER_COMPOSE_FILE) build test
+	docker compose -f $(CI_DOCKER_COMPOSE_FILE) run --rm -e TEST_FILES=$(TEST_FILES) test
 
 docker-test-int: ## Run integration test
 	@echo "Running integration tests..."
-	docker compose -f docker-compose-ci.yml build test-int
-	docker compose -f docker-compose-ci.yml run --rm -e TEST_FILES=$(TEST_FILES) test-int
+	docker compose -f $(CI_DOCKER_COMPOSE_FILE) build test-int
+	docker compose -f $(CI_DOCKER_COMPOSE_FILE) run --rm -e TEST_FILES=$(TEST_FILES) test-int
