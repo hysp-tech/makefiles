70050158fbd1359a1613053be70215e4695cbea8
ZF <fengyu05@gmail.com>
2025-03-15 23:12:07 +0000

    update kube

--- kube.mk
+++ kube.mk
@@ -1,18 +1,18 @@
 ### Command of kube app deployment ###
-kube-deploy: _kube-check-input ## Deploy kube app: make kube-deploy APP=my_app [ENV=dev]
-    @make -C kube/$(APP) deploy
+kube-deploy: _kube-check-input ## Deploy kube app: make kube-deploy APP=my_app [ENV=dev] [DRY_RUN=true]
+	@make -C kube/$(APP) deploy
 
-kube-upgrade: _kube-check-input ## Upgrade kube app: make kube-upgrade APP=my_app [ENV=dev]
-    @cd kube/$(APP) && make upgrade
+kube-upgrade: _kube-check-input ## Upgrade kube app: make kube-upgrade APP=my_app [ENV=dev] [DRY_RUN=true] [TAG=latest]
+	@make -C kube/$(APP) upgrade
 
-kube-delete: _kube-check-input ## Delete app: make kube-delete APP=my_app [ENV=dev]
-    @cd kube/$(APP) && make delete
+kube-delete: _kube-check-input ## Delete app: make kube-delete APP=my_app [ENV=dev] [DRY_RUN=true]
+	@make -C kube/$(APP) delete
 
 kube-show: _kube-check-input ## Show app yaml: make kube-show APP=my_app [ENV=dev]
-    @cd kube/$(APP) && make show
+	@make -C kube/$(APP) show
 
 _kube-check-input:
-    @if [ -z "$(APP)" ] || [ ! -f "kube/$(APP)/Makefile" ]; then \
-        echo "Invalid input: empty APP parameter or kube/$(APP)/Makefile does not exist"; \
-        exit 1; \
-    fi
+	@if [ -z "$(APP)" ] || [ ! -f "kube/$(APP)/Makefile" ]; then \
+		echo "Invalid input: empty APP parameter or kube/$(APP)/Makefile does not exist"; \
+		exit 1; \
+	fi
--- kube_template.mk
+++ kube_template.mk
@@ -1,4 +1,3 @@
-
 # For customized kube
 #KUBECONFIG := ${HOME}/.kube/config
 #KUBECTL := $(KUBECTL_CMD) --kubeconfig=$(KUBECONFIG)
@@ -12,6 +11,10 @@ HELM_CHART ?= chart
 ENV ?= dev
 TAG ?= undefined
 USE_OCI ?= true
+DRY_RUN ?= false
+
+# Define dry run flag based on DRY_RUN value
+HELM_DRY_RUN := $(if $(filter true,$(DRY_RUN)),--dry-run,)
 
 # Define repo path based on USE_OCI
 HELM_REPO := $(if $(filter true,$(USE_OCI)),$(HELM_REPO_URL),$(HELM_REPO_NAME))
@@ -19,45 +22,45 @@ HELM_REPO := $(if $(filter true,$(USE_OCI)),$(HELM_REPO_URL),$(HELM_REPO_NAME))
 # Shared target
 .PHONY: init
 init:  ## Add helm repo
-	@echo "init $(APP)"
-	if [ "$(USE_OCI)" = true ]; then \
-		echo "OCI doesn't need init"; \
-	else \
-		$(HELM) repo add $(HELM_REPO_NAME) $(HELM_REPO_URL); \
-	fi
+    @echo "init $(APP)"
+    if [ "$(USE_OCI)" = true ]; then \
+        echo "OCI doesn't need init"; \
+    else \
+        $(HELM) repo add $(HELM_REPO_NAME) $(HELM_REPO_URL); \
+    fi
 
 
 .PHONY: deploy
 deploy: init ## deploy release with helm
-	@echo "deploy $(APP) in $(ENV) env"
-	$(HELM) install $(APP)-$(ENV) $(HELM_REPO)/$(HELM_CHART) -f values-$(ENV).yaml
+    @echo "deploy $(APP) in $(ENV) env $(if $(filter true,$(DRY_RUN)),[DRY RUN],)"
+    $(HELM) install $(APP)-$(ENV) $(HELM_REPO)/$(HELM_CHART) -f values-$(ENV).yaml $(HELM_DRY_RUN)
 
 
 .PHONY: upgrade
 upgrade:  ## upgrade release with helm
-	@echo "upgrade $(APP) in $(ENV) env"
-	if [ "$(TAG)" != "undefined" ] ; then \
-		$(HELM) upgrade $(APP)-$(ENV) $(HELM_REPO)/$(HELM_CHART) -f values-$(ENV).yaml --set image.tag=$(TAG); \
-	else \
-		$(HELM) upgrade $(APP)-$(ENV) $(HELM_REPO)/$(HELM_CHART) -f values-$(ENV).yaml ; \
-	fi
+    @echo "upgrade $(APP) in $(ENV) env $(if $(filter true,$(DRY_RUN)),[DRY RUN],)"
+    if [ "$(TAG)" != "undefined" ] ; then \
+        $(HELM) upgrade $(APP)-$(ENV) $(HELM_REPO)/$(HELM_CHART) -f values-$(ENV).yaml --set image.tag=$(TAG) $(HELM_DRY_RUN); \
+    else \
+        $(HELM) upgrade $(APP)-$(ENV) $(HELM_REPO)/$(HELM_CHART) -f values-$(ENV).yaml $(HELM_DRY_RUN); \
+    fi
 
 .PHONY: show
 show:  ## Show release with helm
-	@echo "show $(APP) in $(ENV) env"
-	$(HELM) template $(APP)-$(ENV) $(HELM_REPO)/$(HELM_CHART) -f values-$(ENV).yaml
+    @echo "show $(APP) in $(ENV) env"
+    $(HELM) template $(APP)-$(ENV) $(HELM_REPO)/$(HELM_CHART) -f values-$(ENV).yaml
 
 
 .PHONY: delete
 delete:  ## Delete release with helm
-	@if [ "$(NO_INTERACTION)" != "YES" ] ; then \
-		read -p "Do you want to delete $(APP) in $(ENV) env? [y/N]: " answer; \
-		if [ "$$answer" != "y" ]; then \
-			echo "Aborted."; exit 1; \
-		fi \
-	fi 
-	if $(HELM) status $(APP)-$(ENV) > /dev/null 2>&1; then \
-        $(HELM) uninstall $(APP)-$(ENV); \
+    @if [ "$(NO_INTERACTION)" != "YES" ] ; then \
+        read -p "Do you want to delete $(APP) in $(ENV) env? [y/N]: " answer; \
+        if [ "$$answer" != "y" ]; then \
+            echo "Aborted."; exit 1; \
+        fi \
+    fi 
+    if $(HELM) status $(APP)-$(ENV) > /dev/null 2>&1; then \
+        $(HELM) uninstall $(APP)-$(ENV) $(HELM_DRY_RUN); \
     else \
         echo "$(APP) in $(ENV) env is not deployed. Nothing to delete."; \
     fi
